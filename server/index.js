69db58fc28c3fff7c5ad95cf1f081b076b199404
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-06 21:38:26 -0500

    Installed initial dependencies and established connection to server.

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+// const db = require('./db');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`This app is running on a port: ${PORT}`);
+});
