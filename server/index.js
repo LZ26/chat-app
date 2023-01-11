fcd5374f79a57ac6ed9f27f8edba513ca755be39
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-10 19:01:24 -0500

    Connected socket.io with React.

--- server/index.js
+++ server/index.js
@@ -9,12 +9,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const http = require('http').Server(app);
 const cors = require('cors');
+const PORT = process.env.PORT || 3000; //use port from env or 3000 by default
 
 app.use(cors());
 
+//creating real-time connection
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+  },
+});
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+});
+
 app.use('/routes', require('./routes'));
 
-const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`This app is running on a port: ${PORT}`);
 });
