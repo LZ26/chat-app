2e83d9c74e18dca7e427973b1d71d9163d34c930
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-12 21:18:20 -0500

    sequelize fix.

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,19 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/chat-app',
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
   {
     dialect: 'postgres',
   }
 );
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://localhost:5432/chat-app',
+//   {
+//     dialect: 'postgres',
+//   }
+// );
 
 module.exports = db;
