4eb6f5459f31a0e9141d13bca4f7d82f979670b3
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-12 20:57:30 -0500

    Deployment fix v4.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,29 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('database', 'username', 'password', {
-  dialect: 'postgres',
-});
+
+let sequelize;
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.POSTGRES_DB || 'chat-app',
+    process.env.POSTGRES_USER || 'Laziz',
+    '',
+    {
+      host: process.env.PSQL_HOST || 'localhost',
+      dialect: 'postgres',
+      pool: {
+        max: 100,
+        min: 0,
+        idle: 200000,
+        acquire: 1000000,
+      },
+    }
+  );
+}
+
+// const db = new Sequelize('database', 'username', 'password', {
+//   dialect: 'postgres',
+// });
 
 //process.env.DATABASE_URL
 //removed for deployment purposes:
