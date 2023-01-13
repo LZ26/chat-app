c241eaeb51725cd15e0a30bf6e053c421d82c515
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-12 20:53:38 -0500

    Deployment fix v3.

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize('database', 'username', 'password', {
+  dialect: 'postgres',
+});
 
+//process.env.DATABASE_URL
 //removed for deployment purposes:
 //'postgres://localhost:5432/chat-app
 module.exports = db;
