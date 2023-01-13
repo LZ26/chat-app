40b150a31d69973e2e5551f193b0ba9c0e93d35d
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-12 21:13:28 -0500

    updated db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,28 +1,10 @@
-const config = require('config');
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  config.get('postgres.database'),
-  config.get('postgres.username'),
-  config.get('postgres.password'),
+  process.env.DATABASE_URL || 'postgres://localhost:5432/chat-app',
   {
-    host: config.get('postgres.host'),
     dialect: 'postgres',
-    operatorsAliases: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
   }
 );
 
-// const db = new Sequelize('database', 'username', 'password', {
-//   dialect: 'postgres',
-// });
-
-//process.env.DATABASE_URL
-//removed for deployment purposes:
-//'postgres://localhost:5432/chat-app
 module.exports = db;
