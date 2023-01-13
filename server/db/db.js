adbdf3bfc615fa96adb0ca2b7e965d5d6c67dcb6
Laziz Zokirjonov <laziz.zokir26@gmail.com>
2023-01-12 21:04:13 -0500

    Deployment fix attempt v5

--- server/db/db.js
+++ server/db/db.js
@@ -1,25 +1,21 @@
 const Sequelize = require('sequelize');
 
-let sequelize;
-if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.POSTGRES_DB || 'chat-app',
-    process.env.POSTGRES_USER || 'Laziz',
-    '',
-    {
-      host: process.env.PSQL_HOST || 'localhost',
-      dialect: 'postgres',
-      pool: {
-        max: 100,
-        min: 0,
-        idle: 200000,
-        acquire: 1000000,
-      },
-    }
-  );
-}
+const db = new Sequelize(
+  config.get('postgres.database'),
+  config.get('postgres.username'),
+  config.get('postgres.password'),
+  {
+    host: config.get('postgres.host'),
+    dialect: 'postgres',
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 // const db = new Sequelize('database', 'username', 'password', {
 //   dialect: 'postgres',
